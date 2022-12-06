4d6872152d266e93dc2b368116980af81c6f7e91
Steven <slosch99@gmail.com>
2022-12-06 11:06:20 -0500

    Install Redux Toolkit and React-Redux and create counterSlice to check it works

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Counter } from "./redux/features/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
