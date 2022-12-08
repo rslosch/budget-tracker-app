157ec38957e7ba601c2fc87154e247993b28647b
Steven <slosch99@gmail.com>
2022-12-08 17:18:01 -0500

    Seed data and test :through associations. Change attribute on Category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
     has_many :transactions
     has_many :users, through: :transactions
 
-    # accepts_nested_attributes_for :transactions
+    accepts_nested_attributes_for :transactions
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+# CATEGORY = %w[food entertainment medical rent gifts transport] 
+
+#Create Category Instances
+# categories = CATEGORY.each do |cat|
+#     name = cat
+
+#     category = Category.create!(
+#         name: name
+#     )
+# end
+
+# bob = User.find(20)
+# madison = User.find(21)
+# steve = User.find(22)
+
+# 8.times do |i|
+#     bob.transactions.create!(
+#         description: Faker::Lorem.word,
+#         amount: rand(1..250),
+#         category_id: rand(7..12)
+#     )
+# end
+
+# 8.times do |i|
+#     madison.transactions.create!(
+#         description: Faker::Lorem.word,
+#         amount: rand(1..250),
+#         category_id: rand(7..12)
+#     )
+# end
+
+# 8.times do |i|
+#     steve.transactions.create!(
+#         description: Faker::Lorem.word,
+#         amount: rand(1..250),
+#         category_id: rand(7..12)
+#     )
+# end
+
+# CAN NOW ACCESS bob = User.first
+# bob.categories.uniq
+
+# CAN NOW ACCESS food = Category.first
+# food.users.uniq
